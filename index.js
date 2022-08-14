e9e30a8f48cdfd9ba3c2a791f7143076dbb69983
Ben Kurose <ben.kurose@gmail.com>
2022-08-14 12:10:14 -0400

    commit tree 5fe6162a9e9242e26bae3e9be4fe88f03e7f609d

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+function findMatching(drivers, driverName) {
+    return drivers.filter(arrayItem => arrayItem.toLowerCase() === driverName.toLowerCase())
+}
+   
+function fuzzyMatch(drivers, driverName) {
+    return drivers.filter(arrayItem => arrayItem.slice(0, 2) === driverName.slice(0, 2) )
+}
+   
+function matchName(drivers, driverName){
+    return drivers.filter(obj => obj.name === driverName)
+}
+   
+//     return driversArray.filter((obj) => {
+//         let nameNoCase = obj.name.toLowerCase();
+//         let driverNoCase = driverName.toLowerCase();
+//         return nameNoCase === driverNoCase
+//     });
+// };
